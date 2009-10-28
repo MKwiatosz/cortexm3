acf7bfc519202bbae4131b77e7918bc499f2f166
developer.cortex <developer.cortex@09a6722a-bd84-11de-9a97-2195d4080351>
2009-10-28 10:27:54 +0000

    Console function to project.

--- ST/Project/SD_FS/Inc/cmd.h
+++ ST/Project/SD_FS/Inc/cmd.h
@@ -1,29 +1,60 @@
 /******************** (C) COPYRIGHT 2009 developer.cortex **********************
-* File Name          : hw_config.h
+* File Name          : cmd.h
 * Author             : Xu Mingfeng
 * Version            : V1.0.0
 * Date               : 2009-10-28
 * Description        : This file contains all the functions prototypes for
-*                      hardware configure.
+*                      all command.
 *******************************************************************************/
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __HW_CONFIG_H
-#define __HW_CONFIG_H
+#ifndef __CMD_H
+#define __CMD_H
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
 
 /* Exported types ------------------------------------------------------------*/
+typedef void (*cmdHandler)(char *param);
+typedef struct cmd_s {
+  const char   *cmd;
+  cmdHandler   phandler;
+  const char   *phelp;
+  struct cmd_s *pnext;
+} cmd_t;
+
 /* Exported constants --------------------------------------------------------*/
+//Define the console version
+#define  CONSOLE_VER_MAJOR         1
+#define  CONSOLE_VER_SUB           0
+#define  CONSOLE_VER_MINOR         0
+
+#ifndef NULL
+#define NULL  ((void *)0)
+#endif
+
+#ifndef TRUE
+#define TRUE  (1)
+#endif
+#ifndef FALSE
+#define FALSE (0)
+#endif
+
+#ifndef SUCCESS
+#define SUCCESS (0)
+#endif
+#ifndef FAIL
+#define FAIL  (1)
+#endif
+
 /* Exported macro ------------------------------------------------------------*/
 /* External variables --------------------------------------------------------*/
-extern USART_InitTypeDef USART_InitStructure;
-extern GPIO_InitTypeDef  GPIO_InitStructure;
-
 /* Exported functions ------------------------------------------------------- */
-extern void HardwareConfigure(void);
-extern void PrintSysInfo(void);
+extern int  getkey(void);
+extern void cmdHelpInit(void);
+extern void cmdBasicInit(void);
+extern void cmdRegister(cmd_t *bc);
+extern void cmdMonitor(void);
 
-#endif /* __HW_CONFIG_H */
+#endif /* __CMD_H */
 /******************* (C) COPYRIGHT 2009 developer.cortex *******END OF FILE****/
