6858ffedcc5ea9697c0dfdd2ac9f59d398d749d5
developer.cortex <developer.cortex@09a6722a-bd84-11de-9a97-2195d4080351>
2009-10-28 03:51:14 +0000

    1. Added getchar functions; 2. Cleanup project;

--- ST/Project/SD_FS/Src/main.c
+++ ST/Project/SD_FS/Src/main.c
@@ -6,21 +6,12 @@
   * @date    10/26/2009
   * @brief   Main program body
   ******************************************************************************
-  * @copy
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2009 STMicroelectronics</center></h2>
   */ 
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
-#include <stdio.h>
+#include "hw_config.h"
+#include "task.h"
 
 /** @addtogroup Template_Project
   * @{
@@ -30,9 +21,6 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-USART_InitTypeDef USART_InitStructure;
-GPIO_InitTypeDef GPIO_InitStructure;
-
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
@@ -43,53 +31,15 @@ GPIO_InitTypeDef GPIO_InitStructure;
   */
 int main(void)
 {
-  /* Setup STM32 system (clock, PLL and Flash configuration) */
-  SystemInit();
+  /* Configure hardware */
+  HardwareConfigure();
 
-  /* Enable GPIOA and USART1 clock */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_USART1, ENABLE);
+  /* Main task */
+  MainTask();
 
-  /* Configure USART1 Rx (PA.10) as input floating */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  /* Configure USART1 Tx (PA.09) as alternate function push-pull */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  /* USARTx configured as follow:
-        - BaudRate = 115200 baud  
-        - Word Length = 8 Bits
-        - One Stop Bit
-        - No parity
-        - Hardware flow control disabled (RTS and CTS signals)
-        - Receive and transmit enabled
-  */
-  USART_InitStructure.USART_BaudRate = 115200;
-  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-  USART_InitStructure.USART_StopBits = USART_StopBits_1;
-  USART_InitStructure.USART_Parity = USART_Parity_No;
-  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-  /* Configure the USART1 */
-  USART_Init(USART1, &USART_InitStructure);
-  /* Enable the USARTx */
-  USART_Cmd(USART1, ENABLE);
-  printf("\r\n###############################################\n");
-  printf("\r# SD File System                              #\n");
-  printf("\r# author: Xu Mingfeng                         #\n");
-  printf("\r# e-mail: developer.cortex@gmail.com          #\n");
-  printf("\r# date:   2009-10-26                          #\n");
-  printf("\r###############################################\n");
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
+  /* Never run to here ! */
+  printf_1("\r\nMake sure all task done !");
+  while(1){};
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -131,14 +81,22 @@ int  sendchar(int ch)
   return ch;
 }
 
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
 int  getkey(void)
 {
+  /* Loop until the end of transmission */
+  while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET)
+  {
+  }
 
-  return 0;
+  return ((int)USART_ReceiveData(USART1));
 }
 
 /**
   * @}
   */
-
 /******************* (C) COPYRIGHT 2009 STMicroelectronics *****END OF FILE****/
---
+++ ST/Project/SD_FS/Src/task.c
@@ -0,0 +1,37 @@
+/******************** (C) COPYRIGHT 2009 developer.cortex **********************
+* File Name          : task.c
+* Author             : Xu Mingfeng
+* Version            : V1.0.0
+* Date               : 2009-10-28
+* Description        : This file provides main task for project.
+*******************************************************************************/
+
+/* Includes ------------------------------------------------------------------*/
+#include "task.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/*******************************************************************************
+* Function Name  : MainTask
+* Description    : Main task for the project.
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+void MainTask(void)
+{
+  char C;
+  
+  while(1)
+  {
+    C = getchar();
+    if(C == '\r')
+      printf("\n");
+  }
+}
+
+/******************* (C) COPYRIGHT 2009 developer.cortex *******END OF FILE****/
