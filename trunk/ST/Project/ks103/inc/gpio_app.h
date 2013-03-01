ec9cdc8f6ecf89a3451e08e32a0c49a8f064c80a
developer.cortex <developer.cortex@09a6722a-bd84-11de-9a97-2195d4080351>
2013-03-01 06:43:29 +0000

    i2c mode run, need continue.

--- trunk/ST/Project/ks103/inc/gpio_app.h
+++ trunk/ST/Project/ks103/inc/gpio_app.h
@@ -20,11 +20,26 @@
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
+#define SCL_H                          GPIOB->BSRR = GPIO_Pin_6
+#define SCL_L                          GPIOB->BRR  = GPIO_Pin_6
+#define SDA_H                          GPIOB->BSRR = GPIO_Pin_7
+#define SDA_L                          GPIOB->BRR  = GPIO_Pin_7
+#define SCL_READ                       ((GPIOB->IDR& GPIO_Pin_6) >> 6)
+#define SDA_READ                       ((GPIOB->IDR& GPIO_Pin_7) >> 7)
+
 /* External variables --------------------------------------------------------*/
 extern GPIO_InitTypeDef  GPIO_InitStructure;
 
 /* Exported functions --------------------------------------------------------*/
-extern void gpio_config(void);
+extern void    gpio_config(void);
+extern void    gpio_i2c_delay(uint32_t time);
+extern void    gpio_i2c_init(void);
+extern void    gpio_i2c_start(void);
+extern void    gpio_i2c_stop(void);
+extern void    gpio_i2c_ack(void);
+extern void    gpio_i2c_nack(void);
+extern void    gpio_i2c_write(uint8_t data_write);
+extern uint8_t gpio_i2c_read(void);
 
 #endif /* __APP_GPIO_APP_H */
 /********************** (C) COPYRIGHT 2013 AEE  *****************END OF FILE***/
--- trunk/ST/Project/ks103/src/gpio_app.c
+++ trunk/ST/Project/ks103/src/gpio_app.c
@@ -17,6 +17,8 @@
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
+#define I2C_NOP()                      gpio_i2c_delay(200)
+
 /* Private variables ---------------------------------------------------------*/
 GPIO_InitTypeDef  GPIO_InitStructure;
 
@@ -30,7 +32,8 @@ GPIO_InitTypeDef  GPIO_InitStructure;
 void gpio_config(void)
 {
 	/* Enable GPIOC, GPIOE */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB
+	                     | RCC_APB2Periph_GPIOC 
 		                 | RCC_APB2Periph_GPIOD 
 		                 | RCC_APB2Periph_GPIOE, ENABLE);
 
@@ -51,6 +54,161 @@ void gpio_config(void)
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+	/* Configure I2C1 pins: SCL and SDA */
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 | GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
+
+/******************************************************************************/
+/**
+  * @fn     void gpio_i2c_delay(void)
+  * @brief  gpio i2c delay.
+  * @retval None
+  */
+void gpio_i2c_delay(uint32_t time)
+{
+	uint32_t timeout = time;
+	if(timeout){
+		while(timeout--){
+		}
+	}
+}
+
+/******************************************************************************/
+/**
+  * @fn     void gpio_i2c_init(void)
+  * @brief  gpio i2c init.
+  * @retval None
+  */
+void gpio_i2c_init(void)
+{
+	SDA_H;
+	SCL_H;
+}
+
+/******************************************************************************/
+/**
+  * @fn     void gpio_i2c_start(void)
+  * @brief  gpio i2c start.
+  * @retval None
+  */
+void gpio_i2c_start(void)
+{
+	SDA_H;
+	I2C_NOP();
+	SCL_H;
+	I2C_NOP();
+	SDA_L;
+	I2C_NOP();
+}
+
+/******************************************************************************/
+/**
+  * @fn     void gpio_i2c_stop(void)
+  * @brief  gpio i2c stop.
+  * @retval None
+  */
+void gpio_i2c_stop(void)
+{
+	SDA_L;
+	I2C_NOP();
+	SCL_H;
+	I2C_NOP();
+	SDA_H;
+	I2C_NOP();
+}
+
+/******************************************************************************/
+/**
+  * @fn     void gpio_i2c_ack(void)
+  * @brief  gpio i2c ack.
+  * @retval None
+  */
+void gpio_i2c_ack(void)
+{
+	uint32_t timeout = 0;
+
+	SCL_H;
+	I2C_NOP();
+	while(SDA_READ == 1 && timeout < 2000){
+		timeout++;
+	}
+	SCL_L;
+	I2C_NOP();
+}
+
+
+/******************************************************************************/
+/**
+  * @fn     void gpio_i2c_nack(void)
+  * @brief  gpio i2c nack.
+  * @retval None
+  */
+void gpio_i2c_nack(void)
+{
+	SDA_H;
+	I2C_NOP();
+	SCL_H;
+	I2C_NOP();
+	SCL_L;
+	I2C_NOP();
+}
+
+/******************************************************************************/
+/**
+  * @fn     void gpio_i2c_write(void)
+  * @brief  gpio i2c write.
+  * @retval None
+  */
+void gpio_i2c_write(uint8_t data_write)
+{
+	uint8_t i;
+
+	SCL_L;
+	for(i = 0; i < 8; i++){
+		if(data_write & 0x80)
+			SDA_H;
+		else
+			SDA_L;
+		data_write = data_write << 1;
+		I2C_NOP();
+		SCL_H;
+		I2C_NOP();
+		SCL_L;
+		I2C_NOP();
+	}
+	SCL_H;
+	I2C_NOP();
+}
+
+/******************************************************************************/
+/**
+  * @fn     void gpio_i2c_read(void)
+  * @brief  gpio i2c read.
+  * @retval None
+  */
+uint8_t gpio_i2c_read(void)
+{
+	uint8_t i, data_read = 0;
+
+	SCL_L;
+	I2C_NOP();
+	SDA_H;
+	I2C_NOP();
+	for(i = 0; i < 8; i++){
+		SCL_H;
+		I2C_NOP();
+		data_read = data_read << 1;
+		if(SDA_READ == 1)
+			data_read++;
+		SCL_L;
+		I2C_NOP();
+	}
+
+	return data_read;
 }
 
 /********************** (C) COPYRIGHT 2013 AEE  *****************END OF FILE***/
